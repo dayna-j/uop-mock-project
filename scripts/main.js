03256cbf4692e4753e50324e0448d9c00a30d640
Dayna <daynajblackwell@gmail.com>
2018-11-14 11:50:32 -0700

    Developing JavaScript functions to extract data from form. jQuery's find method is used to select every input, select and textarea on the form.

---
+++ scripts/main.js
@@ -0,0 +1,59 @@
+// window.addEventListener('load', 
+//     function() { 
+    
+    
+    // get the form data
+    let getFormData = function() {
+        let myForm = $('#development-request-form');
+        myFormInputs = myForm.find('input,select,textarea');
+        let obj = {};
+        
+        myFormInputs.each((index,input)=>{
+            console.log(input);
+        });
+
+        // console.log(formElements);
+        
+        // let obj = {};
+        // for(element in formElements) {
+        //     if(formElements[element].nodeName === 'INPUT') {
+        //         obj[formElements[element].id] = formElements[element].value
+        //     }
+        // }
+        // console.log(obj);
+        // return obj;
+
+        // let data = {};
+
+        // $('#development-request-form').filter(':input').each( (input) => {
+        //     const $id = $(this).attr('id');
+        //     const $value = $(this).attr('value');
+        //     data[$id] = $value;
+        // });
+        // console.log(data);
+        // return data;
+
+        return obj;
+
+
+     }
+
+    // post the form data
+    let postData = function(event) {
+        event.preventDefault();
+        // alert('postData working');
+        
+        let formData = getFormData();
+        console.log(formData);
+    }
+
+
+
+
+    let URL = 'https://jsonplaceholder.typicode.com/posts';
+    document.getElementById('development-request-form').addEventListener('submit', postData);
+
+
+
+
+//   }, false);
