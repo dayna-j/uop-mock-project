2fedea9092bfd389351e5ffddc7bdd046d2a680f
Dayna <daynajblackwell@gmail.com>
2018-11-14 17:40:29 -0700

    postData is successfully posting form data to JSONPlaceholder service. Response is logging to console.

--- scripts/main.js
+++ scripts/main.js
@@ -1,62 +1,53 @@
 // window.addEventListener('load', 
-//     function() { 
+    // function() { 
+    let URL = 'https://jsonplaceholder.typicode.com/posts';
     
-    
+        // post the form data
+    let postData = function(event) {
+        event.preventDefault();
+        // alert('postData working');
+        let formData = getFormData();
+        console.log(formData);
+        let postOptions = {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json'
+              }
+        };
+        fetch(URL, postOptions)
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch((err)=>console.log(err));
+    }
     // get the form data
     let getFormData = function() {
         let myForm = $('#development-request-form');
         myFormInputs = myForm.find('input,select,textarea');
-        let formData = [];
+        let formData = {};
         
-        myFormInputs.each((index,input)=>{
-            console.log(input);
-            formData.push({
-                id: input.id,
-                value: input.value
-             });
+        myFormInputs.each( (index,input) => {
+            // console.log(input);
+            // formData.push({
+                // id: input.id,
+                // value: input.value
+            //  });
+            formData[input.id] = input.value
         });
-        // console.log(formElements);
-        
-        // let obj = {};
-        // for(element in formElements) {
-        //     if(formElements[element].nodeName === 'INPUT') {
-        //         obj[formElements[element].id] = formElements[element].value
-        //     }
-        // }
-        // console.log(obj);
-        // return obj;
-
-        // let data = {};
-
-        // $('#development-request-form').filter(':input').each( (input) => {
-        //     const $id = $(this).attr('id');
-        //     const $value = $(this).attr('value');
-        //     data[$id] = $value;
-        // });
-        // console.log(data);
-        // return data;
-
         return formData;
     }
 
 
-
-    // post the form data
-    let postData = function(event) {
-        event.preventDefault();
-        // alert('postData working');
-        
-        let formData = getFormData();
-        console.log(formData);
-    }
+    document.getElementById('development-request-form').addEventListener('submit', postData);    
 
 
-
-
-    let URL = 'https://jsonplaceholder.typicode.com/posts';
-    document.getElementById('development-request-form').addEventListener('submit', postData);
 
 
 
     
-//   }, false)}
+    
+
+
+
+    
+//   }, false)};
